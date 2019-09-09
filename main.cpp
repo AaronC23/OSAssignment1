9ef89a449df6dd46614ac6adf58578ac9b06b4e7
AaronC23 <Aaron.Clarky23@gmail.com>
2019-09-09 15:19:31 +0930

    initial commit

--- main.cpp
+++ main.cpp
@@ -14,67 +14,50 @@ private:
 		int tickets;
 		int queue;
 		int interruptCount;
+		int currentRuns;
 		int queuePos; //Might not need this
 		int runningTime; //Duration of buying tickets
 		int waitingTime; //Duration of waiting
 		int terminationTime; //Time when process bought all tickets
-		int currentRuns;
 public:
 	Customer(){
-			
-	}
-
-	void process(){
-		currentRuns++;
-		if(currentRuns == 3){
-			currentRuns = 0;
-			priority++;
-		}
-	}
-};
-
-// stores customers and processes the queue
-class CustomerQueue{
-private:
-	vector<Customer> queue;
-	vector<Customer> executingQueue;
-public:
-	CustomerQueue(){
-	}
-
-	int queueSize(){
-		return queue.size();
-	}
-
-	void addCustomer(Customer cust){
-		queue.push_back(cust);
-	}
-
-	void moveToExecutingQueue(int custID){
-
-	}
-
-	void processQueue(){
-
-	}
-};
-
-void changeQueue(CustomerQueue* q1, CustomerQueue* q2, int custID){
-	for(int i=0;i<q1.queueSize();i++){
 		
 	}
-}
+
+};
 
 int main(int argc, char *argv[]){
-	Customer customers;
-	if(!infile) {
-    	cout << "Cannot open input file.\n";
-   		return 1;
-  	}
-	customers.getCustomers(argv[1]);
-
-	CustomerQueue queueOne;
-	CustomerQueue queueTwo;
-
+	char str[256];
+	string delimiter = " ";
+	size_t pos = 0;
+	string token;
+	ifstream infile(argv[1]);
+	while(infile) {
+		Customer customer;
+	   	infile.getline(str, 256);  // delim defaults to '\n'
+	   	while((pos = s.find(delimiter)) != string:npos){
+			token = s.substr(0,pos);
+			cout << token << endl;
+			s.erase(0,pos+delimiter.length());
+		}
+	   	if(priority<=3){
+	   		queueOne.addCustomer(customer);
+	   	} else {
+	   		queueTwo.addCustomer(customer);
+	   	}
+	   	if(infile) cout << str << endl;
+	}
 	return 0;
 }
+
+std::string s = "scott>=tiger>=mushroom";
+std::string delimiter = ">=";
+
+size_t pos = 0;
+std::string token;
+while ((pos = s.find(delimiter)) != std::string::npos) {
+    token = s.substr(0, pos);
+    std::cout << token << std::endl;
+    s.erase(0, pos + delimiter.length());
+}
+std::cout << s << std::endl;
